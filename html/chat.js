5ae3979d63f3247b5027523fde230646f5d9e441
negavite <ajlo@ucsd.edu>
2018-02-16 13:37:46 -0800

    Add files via upload

---
+++ html/chat.js
@@ -0,0 +1,8 @@
+document.getElementById("myBtn").addEventListener("click", function(){
+                                                  var div = document.createElement("div");
+                                                  div.style.width = "800px";
+                                                  div.style.height = "50px";
+                                                  div.style.border = "1px solid #000000";
+                                                  div.innerHTML = document.getElementById("myTxtbox").value;
+                                                  document.getElementById("msgs").appendChild(div);
+                                                  });
