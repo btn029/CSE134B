9c9bea893124830674cb9589f6754ed57e2b3fd0
Brian Vu <brian_vu2594@yahoo.com>
2018-02-16 13:15:41 -0800

    2/16 updated

--- js/entry.js
+++ js/entry.js
@@ -1,3 +1,4 @@
+
 document.getElementById("myBtn").addEventListener("click", function(){
     var div = document.createElement("div");
     div.style.width = "800px";
@@ -7,7 +8,16 @@ document.getElementById("myBtn").addEventListener("click", function(){
     document.getElementById("msgs").appendChild(div);
 });
 
-function saveData(){
+function saveDataClient(){
 	var input = document.getElementById("number");
 	localStorage.setItem("ratingVal", input.value);
+}
+
+function appendCommentClient(){
+	var div = document.createElement("div");
+    div.style.width = "800px";
+    div.style.height = "100px";
+    div.style.border = "thick solid #000000";
+    div.innerHTML = document.getElementById("myTxtbox").value;
+    document.getElementById("msgs").appendChild(div);
 }
